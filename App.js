7b3bb12e451c0e1ab3fdf1cda6c4562d27ebfc70
hanur92 <rkdgksdjfwkd92@gmail.com>
2020-10-30 18:31:55 +0900

    🔧 move to git

---
+++ App.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import Decrement from "./ component/Decrement";
+import Increment from "./ component/Increment";
+import Buttons from "./ component/Buttons";
+// import logo from "./logo.svg";
+// import './App.css';
+
+function App() {
+  // const number = useState(0);
+  return (
+    <div>
+      <Buttons></Buttons>
+      {/* <Increment></Increment>
+      <Decrement></Decrement> */}
+      {/* <Increment />
+      {number}
+      <Decrement /> */}
+    </div>
+  );
+}
+
+export default App;
